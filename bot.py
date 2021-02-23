33b1a967c3b85d9ca3f4ad7346132ec97ab3d7e5
Jacob Sanders <sanders.j4@gmail.com>
2021-02-22 23:48:17 -0500

    All response messages are now DMs

--- bot.py
+++ bot.py
@@ -24,6 +24,12 @@ def mentionToId(mention):
         userId = userId[1:]
     return int(userId)
 
+
+async def sendDM(user, message):
+    await user.create_dm()
+    await user.dm_channel.send(message)
+
+
 @bot.event
 async def on_ready():
     print(f'{bot.user.name} has connected to Discord!')
@@ -44,181 +50,188 @@ async def clean_db(ctx):
 @bot.command(name='mk_account', help='Create a new mutual credit account | !mk_account')
 @commands.has_role('member')
 async def mk_account(ctx):
+    member = ctx.guild.get_member(ctx.author.id)
     try:
         cs.createAccount(ctx.author.id)
     except Exception as e:
-        await ctx.send(str(e))
+        await sendDM(member, str(e))
         print(e)
     else:
-        await ctx.send('Mutual credit account created!')
+        await sendDM(member, 'Mutual credit account created!')
 
 
 @bot.command(name='balance', help='Show your account balance | !get_account_min_balance')
 @commands.has_role('member')
 async def balance(ctx):
+    member = ctx.guild.get_member(ctx.author.id)
     balance = None
     try:
         balance = cs.getBalance(ctx.author.id)
     except Exception as e:
-        await ctx.send(str(e))
+        await sendDM(member, str(e))
         print(e)
         raise
     else:
-        await ctx.send(f'Account balance: ${balance}')
+        await sendDM(member, f'Account balance: ${balance}')
 
 
 # Anything special needed for multiword arguments (space delimited but quoted)?
 @bot.command(name='mk_offer', help='Create a new offer to your account | !mk_offer DESC PRICE TITLE')
 @commands.has_role('member')
 async def mk_offer(ctx, description, price: int, title):
+    member = ctx.guild.get_member(ctx.author.id)
     offerId = None
     try:
         offerId = cs.createOffer(ctx.author.id, description, price, title)
     except Exception as e:
-        await ctx.send(str(e))
+        await sendDM(member, str(e))
         print(e)
     else:
-        await ctx.send(f'Offer ID: {offerId}')
+        await sendDM(member, f'Offer created with ID {offerId}')
 
 
 @bot.command(name='add_cats', help='Add categories to an existing offer | !add_cats OFFER_ID CAT1 CAT2 CAT3...')
 @commands.has_role('member')
 async def add_cats(ctx, offer_id, *args):
+    member = ctx.guild.get_member(ctx.author.id)
     cnt = 0
 
     for category in args:
         try:
             cs.addCategoryToOffer(offer_id, category)
         except Exception as e:
-            await ctx.send(str(e))
+            await sendDM(member, str(e))
             print(e)
         else:
             cnt += 1
 
-    await ctx.send(f'{cnt}/{len(args)} categories added to offer')
+    await sendDM(member, f'{cnt}/{len(args)} categories added to offer with ID {offer_id}')
 
 
 @bot.command(name='ls_cats', help='List categories associated with an existing offer | !ls_cats OFFER_ID')
 @commands.has_role('member')
 async def ls_cats(ctx, offer_id):
+    member = ctx.guild.get_member(ctx.author.id)
     try:
         categories = cs.getOfferCategories(offer_id)
     except Exception as e:
-        await ctx.send(str(e))
+        await sendDM(member, str(e))
         print(e)
     else:
         if len(categories) > 0:
             categories = ', '.join(categories)
-        await ctx.send(f'Offer {offer_id} categories: {categories}')
+        await sendDM(member, f'Offer {offer_id} categories: {categories}')
 
 
 @bot.command(name='rm_cats', help='Remove categories from an existing offer | !rm_cats OFFER_ID CAT1 CAT2 CAT3...')
 @commands.has_role('member')
 async def rm_cats(ctx, offer_id, *args):
+    member = ctx.guild.get_member(ctx.author.id)
     cnt = 0
 
     for category in args:
         try:
             cs.removeCategoryFromOffer(offer_id, category)
         except Exception as e:
-            await ctx.send(str(e))
+            await sendDM(member, str(e))
             print(e)
         else:
             cnt += 1
 
-    await ctx.send(f'{cnt}/{len(args)} categories removed from offer')
+    await sendDM(member, f'{cnt}/{len(args)} categories removed from offer with ID {offer_id}')
 
 
 @bot.command(name='rm_offer', help='Remove an offer from your account | !rm_offer OFFER_ID')
 @commands.has_role('member')
 async def rm_offer(ctx, offer_id):
+    member = ctx.guild.get_member(ctx.author.id)
     try:
         seller_id = cs.getOfferSeller(offer_id)
         if ctx.author.id != seller_id:
-            raise Exception('User attempted to delete offer from another user\'s account')
+            await sendDM(member, 'You can\'t delete an offer from another member\'s account!')
         cs.deleteOffer(offer_id)
     except Exception as e:
         await ctx.send(str(e))
         print(e)
     else:
-        await ctx.send('Offer has been removed!')
+        await sendDM(member, 'Offer has been removed!')
 
 
 # Output needs better formatting
 @bot.command(name='ls_offers', help='Get offers for a seller\'s account | !ls_offers SELLER')
 @commands.has_role('member')
 async def ls_offers(ctx, seller):
+    member = ctx.guild.get_member(ctx.author.id)
     seller_id = mentionToId(seller)
     offers = None
     try:
         offers = cs.getOffers(seller_id)
     except Exception as e:
-        await ctx.send(str(e))
+        await sendDM(member, str(e))
         print(e)
     else:
-        await ctx.send(f'{offers}')
+        await sendDM(member, f'{offers}')
 
 
 @bot.command(name='buy', help='Send a request to buy an offer from a seller | !buy OFFER_ID')
 @commands.has_role('member')
 async def mk_buy(ctx, offer_id):
+    member = ctx.guild.get_member(ctx.author.id)
     tx_id = None
     try:
         tx_id = cs.createTransaction(ctx.author.id, offer_id)
     except Exception as e:
-        await ctx.send(str(e))
+        await sendDM(member, str(e))
         print(e)
     else:
-        await ctx.send(f'Transaction ID: {tx_id}')
+        await sendDM(member, f'Transaction ID: {tx_id}')
         #seller = discord.utils.get(guild.members, name=sellerName)
         seller_id = cs.getOfferSeller(offer_id)
         seller = ctx.guild.get_member(seller_id)
         tx = cs.getTransaction(tx_id)
-        await seller.create_dm()
-        await seller.dm_channel.send(
-            f'New buy request:\n'
-            f'{tx}'
-        )
+        await sendDM(seller, f'New buy request from {ctx.author.name} for offer with ID {offer_id}:\n{tx}')
 
 
 @bot.command(name='approve', help='Approve a buy request from another user | !approve TX_ID')
 @commands.has_role('member')
 async def approve_tx(ctx, tx_id):
+    member = ctx.guild.get_member(ctx.author.id)
     balance = None
     try:
         cs.approveTransaction(tx_id)
     except Exception as e:
-        await ctx.send(str(e))
+        await sendDM(member, str(e))
         print(e)
-        raise
     else:
-        await ctx.send(f'Transaction approved!')
+        await sendDM(member, f'Transaction {tx_id} approved!')
         balance = cs.getBalance(ctx.author.id)
-        await ctx.send(f'New account balance: ${balance}')
+        await sendDM(member, f'New account balance: ${balance}')
 
 
 @bot.command(name='cancel', help='Cancel your pending buy request | !cancel TX_ID')
 @commands.has_role('member')
 async def cancel_tx(ctx, tx_id):
+    member = ctx.guild.get_member(ctx.author.id)
     try:
         cs.cancelTransaction(tx_id)
     except Exception as e:
-        await ctx.send(str(e))
+        await sendDM(member, str(e))
         print(e)
     else:
-        await ctx.send(f'Transaction cancelled!')
+        await sendDM(member, f'Transaction {tx_id} cancelled!')
 
 
 @bot.command(name='deny', help='Deny a request to buy | !deny TX_ID')
 @commands.has_role('member')
 async def deny_tx(ctx, tx_id):
+    member = ctx.guild.get_member(ctx.author.id)
     try:
         cs.denyTransaction(tx_id)
     except Exception as e:
-        await ctx.send(str(e))
+        await sendDM(member, str(e))
         print(e)
     else:
-        await ctx.send(f'Transaction denied!')
+        await sendDM(member, f'Transaction {tx_id} denied!')
 
 
 bot.run(TOKEN)
